f4f77c0ca9c4d9aa3eef48534da4e00e8ba337e1
jerry.y <jerry.y@binance.com>
2021-12-30 20:34:20 +0800

    nothing

--- src/day2/1/index.js
+++ src/day2/1/index.js
@@ -11,30 +11,24 @@ for(var i = 0; i < dive.length; ++i) {
 
     if (word === "forward") {
         diveForward += number;
-        // test log incoming to see if THIS HECKIN WORKS
         console.log(diveForward);
     } else if (word === "down") {
         diveDown += number;
-        // test log incoming to see if THIS HECKIN WORKS
         console.log(diveDown);
     } else if (word === "up") {
         diveUp += number;
-        // test log incoming to see if THIS HECKIN WORKS
         console.log(diveUp);
     };
 };
 
-// I'm sorry dear trees but more DOES THIS HECKIN WORK logs incoming
 console.log(diveForward);
 console.log(diveDown);
 console.log(diveUp);
 
-// Now the actual work
 var horizontalPos = diveForward;
 var depthPos = diveUp - diveDown;
 var multipliedPos = horizontalPos * depthPos;
 
-// GIMME ANSWER PLZ
 console.log(multipliedPos);
 
 
